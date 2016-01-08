9ef87b7a6234d7e6ceb1a9d86a1dcf7898e1d78b
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-09 01:31:13 +0200

    add class Connection

---
+++ include/Connection.h
@@ -0,0 +1,25 @@
+#ifndef _SQLDB_CONNECTION_H_
+#define _SQLDB_CONNECTION_H_
+
+#include <string>
+
+namespace sqldb {
+  class Connection {
+  public:
+    Connection() { }
+    Connection(const Connection & other) = delete;
+    virtual ~Connection() { }
+    Connection & operator=(const Connection & other) = delete;
+    
+    virtual std::shared_ptr<SQLStatement> prepare(const std::string & query) = 0;
+    virtual void begin();
+    virtual void commit();
+    virtual void rollback();
+    virtual unsigned int execute(const char * query);
+    virtual bool ping() { return true; }    
+    
+    unsigned int execute(const std::string & query) { return execute(query.c_str()); }
+  };
+};
+
+#endif
---
+++ src/Connection.cpp
@@ -0,0 +1,25 @@
+#include "Connection.h"
+
+using namespace std;
+using namespace sqldb;
+
+unsigned int
+Connection::execute(const char * query) {
+  auto stmt = prepare(query);
+  return stmt->execute();
+}
+
+void
+Connection::begin() {
+  execute("BEGIN TRANSACTION");
+}
+
+void
+Connection::commit() {
+  execute("COMMIT");
+}
+
+void
+Connection::rollback() {
+  execute("ROLLBACK");
+}
