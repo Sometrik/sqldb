742a68578a3e10cf970aa1069ca196a5f7e4a024
Mikael Rekola <mikael.rekola@gmail.com>
2022-04-11 23:55:14 +0300

    refactor, add MemoryStorage to make copies point to the original data

--- include/MemoryTable.h
+++ include/MemoryTable.h
@@ -8,50 +8,36 @@
 #include <memory>
 
 namespace sqldb {
-  class Cursor;
+  class MemoryStorage;
   
   class MemoryTable : public Table {
   public:
-    MemoryTable(bool numeric_key = false) : numeric_key_(numeric_key) { }
+    MemoryTable(bool numeric_key = false);
 
     bool hasNumericKey() const override { return numeric_key_; }
 
     std::unique_ptr<Table> copy() const override { return std::make_unique<MemoryTable>(*this); }
 
-    void addColumn(std::string name, sqldb::ColumnType type, bool unique = false) override {
-      header_row_.push_back(std::tuple(type, std::move(name), unique));
-    }
+    void addColumn(std::string name, sqldb::ColumnType type, bool unique = false) override;
     
     std::unique_ptr<Cursor> addRow(const std::string & key) override;
-    std::unique_ptr<Cursor> addRow() override { return addRow(std::to_string(++auto_increment_)); }
+    std::unique_ptr<Cursor> addRow() override;
             
     std::unique_ptr<Cursor> seekBegin() override;    
     std::unique_ptr<Cursor> seek(const std::string & key) override;
     
-    int getNumFields() const override { return static_cast<int>(header_row_.size()); }
-    int getNumRows() const { return static_cast<int>(data_.size()); }
-    ColumnType getColumnType(int column_index) const override {
-      auto idx = static_cast<size_t>(column_index);
-      return idx < header_row_.size() ? std::get<0>(header_row_[idx]) : ColumnType::UNDEF;
-    }
-
-    std::string getColumnName(int column_index) const {
-      auto idx = static_cast<size_t>(column_index);
-      return idx < header_row_.size() ? std::get<1>(header_row_[idx]) : "";
-    }
-
-    bool isColumnUnique(int column_index) const {
-      auto idx = static_cast<size_t>(column_index);
-      return idx < header_row_.size() ? std::get<2>(header_row_[idx]) : false;
-    }
+    int getNumFields() const override;
+    int getNumRows() const;
+    
+    ColumnType getColumnType(int column_index) const override;
+    std::string getColumnName(int column_index) const;
+    bool isColumnUnique(int column_index) const;
     
     void append(Table & other) override;
           
   private:
     bool numeric_key_;
-    std::unordered_map<std::string, std::vector<std::string> > data_;
-    std::vector<std::tuple<ColumnType, std::string, bool> > header_row_;
-    long long auto_increment_ = 0;
+    std::shared_ptr<MemoryStorage> storage_;
   };
 };
 
--- src/MemoryTable.cpp
+++ src/MemoryTable.cpp
@@ -7,6 +7,46 @@
 using namespace std;
 using namespace sqldb;
 
+class sqldb::MemoryStorage {
+public:
+  MemoryStorage() { }
+
+  std::unique_ptr<Cursor> seek(const std::string & key);
+  std::unique_ptr<Cursor> seekBegin();
+  std::unique_ptr<Cursor> addRow(const std::string & key);
+  
+  std::unique_ptr<Cursor> addRow() {
+    return addRow(std::to_string(++auto_increment_));
+  }
+
+  void addColumn(std::string name, sqldb::ColumnType type, bool unique = false) {
+    header_row_.push_back(std::tuple(type, std::move(name), unique));
+  }
+  
+  int getNumFields() const { return static_cast<int>(header_row_.size()); }
+  int getNumRows() const { return static_cast<int>(data_.size()); }
+
+  ColumnType getColumnType(int column_index) const {
+    auto idx = static_cast<size_t>(column_index);
+    return idx < header_row_.size() ? std::get<0>(header_row_[idx]) : ColumnType::UNDEF;
+  }
+
+  std::string getColumnName(int column_index) const {
+    auto idx = static_cast<size_t>(column_index);
+    return idx < header_row_.size() ? std::get<1>(header_row_[idx]) : "";
+  }
+
+  bool isColumnUnique(int column_index) const {
+    auto idx = static_cast<size_t>(column_index);
+    return idx < header_row_.size() ? std::get<2>(header_row_[idx]) : false;
+  }
+
+private:
+  std::unordered_map<std::string, std::vector<std::string> > data_;
+  std::vector<std::tuple<ColumnType, std::string, bool> > header_row_;
+  long long auto_increment_ = 0;
+};
+
 class MemoryTableCursor : public Cursor {
 public:
   MemoryTableCursor(std::unordered_map<std::string, std::vector<std::string> > * data,
@@ -104,21 +144,17 @@ private:
 };
 
 std::unique_ptr<Cursor>
-MemoryTable::addRow(const std::string & key) {  
-  assert(!key.empty());
+MemoryStorage::seek(const std::string & key) {
   auto it = data_.find(key);
-  if (it == data_.end()) {
-    data_[key] = std::vector<std::string>();
-    it = data_.find(key);
+  if (it != data_.end()) {
+    return std::make_unique<MemoryTableCursor>(&data_, &header_row_, &auto_increment_, move(it));
+  } else {
+    return std::unique_ptr<Cursor>(nullptr);
   }
-  cerr << "addRow: " << key << " (size = " << data_.size() << ")\n";
-
-  assert(it != data_.end());
-  return std::make_unique<MemoryTableCursor>(&data_, &header_row_, &auto_increment_, move(it));
 }
 
 std::unique_ptr<Cursor>
-MemoryTable::seekBegin() {
+MemoryStorage::seekBegin() {
   auto it = data_.begin();
   if (it != data_.end()) {
     return std::make_unique<MemoryTableCursor>(&data_, &header_row_, &auto_increment_, move(it));
@@ -128,21 +164,77 @@ MemoryTable::seekBegin() {
 }
 
 std::unique_ptr<Cursor>
-MemoryTable::seek(const std::string & key) {
+MemoryStorage::addRow(const std::string & key) {
+  assert(!key.empty());
   auto it = data_.find(key);
-  if (it != data_.end()) {
-    return std::make_unique<MemoryTableCursor>(&data_, &header_row_, &auto_increment_, move(it));
-  } else {
-    return std::unique_ptr<Cursor>(nullptr);
+  if (it == data_.end()) {
+    data_[key] = std::vector<std::string>();
+    it = data_.find(key);
   }
+  
+  assert(it != data_.end());
+  return std::make_unique<MemoryTableCursor>(&data_, &header_row_, &auto_increment_, move(it));
+}
+
+MemoryTable::MemoryTable(bool numeric_key)
+  : numeric_key_(numeric_key), storage_(make_shared<MemoryStorage>()) { }
+
+void
+MemoryTable::addColumn(std::string name, sqldb::ColumnType type, bool unique) {
+  storage_->addColumn(name, type, unique);
+}
+
+std::unique_ptr<Cursor>
+MemoryTable::addRow(const std::string & key) {
+  return storage_->addRow(key);
+}
+
+std::unique_ptr<Cursor>
+MemoryTable::addRow() {
+  return storage_->addRow();
+}
+
+std::unique_ptr<Cursor>
+MemoryTable::seekBegin() {
+  return storage_->seekBegin(); 
+}
+
+std::unique_ptr<Cursor>
+MemoryTable::seek(const std::string & key) {
+  return storage_->seek(key);
+}
+
+int
+MemoryTable::getNumFields() const {
+  return storage_->getNumFields();
+}
+
+int
+MemoryTable::getNumRows() const {
+  return storage_->getNumRows();
+}
+
+ColumnType
+MemoryTable::getColumnType(int column_index) const {
+  return storage_->getColumnType(column_index);
+}
+
+std::string
+MemoryTable::getColumnName(int column_index) const {
+  return storage_->getColumnName(column_index);
+}
+
+bool
+MemoryTable::isColumnUnique(int column_index) const {
+  return storage_->isColumnUnique(column_index);
 }
 
 void
 MemoryTable::append(Table & other) {
-  // FIXME
-  header_row_.clear();
-  for (int i = 0; i < other.getNumFields(); i++) {
-    header_row_.push_back(std::tuple(other.getColumnType(i), other.getColumnName(i), other.isColumnUnique(i)));
+  if (!getNumFields()) { // FIXME 
+    for (int i = 0; i < other.getNumFields(); i++) {
+      addColumn(other.getColumnName(i), other.getColumnType(i), other.isColumnUnique(i));
+    }
   }
   
   if (auto cursor = other.seekBegin()) {
@@ -155,3 +247,4 @@ MemoryTable::append(Table & other) {
     } while (cursor->next());
   }
 }
+
