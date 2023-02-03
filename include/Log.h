0cdcefb2c843912c993468f72919aacb750770ef
Mikael Rekola <mikael.rekola@gmail.com>
2023-02-03 18:14:38 +0200

    add mutex, add missing moves

--- include/Log.h
+++ include/Log.h
@@ -4,6 +4,7 @@
 #include "Key.h"
 
 #include <vector>
+#include <mutex>
 
 namespace sqldb {
   class Log {
@@ -12,22 +13,30 @@ namespace sqldb {
     
     Log() { }
 
-    void add(sqldb::Key key) { data_.emplace_back(Event::ADD, key); }
-    void remove(sqldb::Key key) { data_.emplace_back(Event::REMOVE, key); }
-    size_t size() const { return data_.size(); }
+    void add(sqldb::Key key) {
+      std::lock_guard<std::mutex> guard(mutex_);
+      data_.emplace_back(Event::ADD, std::move(key));
+    }
+    void remove(sqldb::Key key) {
+      std::lock_guard<std::mutex> guard(mutex_);
+      data_.emplace_back(Event::REMOVE, std::move(key));
+    }
+    size_t size() const {
+      std::lock_guard<std::mutex> guard(mutex_);
+      return data_.size();
+    }
 
-    const std::pair<Event, sqldb::Key> & getEvent(int row) const {
+    std::pair<Event, sqldb::Key> getEvent(int row) const {
+      std::lock_guard<std::mutex> guard(mutex_);
       if (row >= 0 && row < static_cast<int>(data_.size())) {
 	return data_[row];
-      } else {
-	return null_row;
       }
+      return std::pair<Event, sqldb::Key>();      
     }
     
   private:
     std::vector<std::pair<Event, sqldb::Key>> data_;
-
-    static inline std::pair<Event, sqldb::Key> null_row;
+    mutable std::mutex mutex_;
   };
 };
 
