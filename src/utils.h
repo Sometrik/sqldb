98dee02697d4aedf5ed544e19e3ce1d9baef3bb7
Mikael Rekola <mikael.rekola@gmail.com>
2023-08-11 15:45:21 +0300

    add result code

--- src/utils.h
+++ src/utils.h
@@ -3,8 +3,10 @@
 
 #include <utf8proc.h>
 
-static inline std::string normalize_nfc(std::string_view input) {
-  if (!input.empty()) {
+static inline std::pair<std::string, int> normalize_nfc(std::string_view input) {
+  if (input.empty()) {
+    return std::pair(std::string(), 0);
+  } else {
     utf8proc_uint8_t * dest = nullptr;
     // option UTF8PROC_STRIPCC is not used since it would remove tabs from tsv files
     auto options = utf8proc_option_t(UTF8PROC_IGNORE | UTF8PROC_COMPOSE | UTF8PROC_NLF2LF | UTF8PROC_LUMP );
@@ -16,12 +18,12 @@ static inline std::string normalize_nfc(std::string_view input) {
     if (s >= 0) {
       std::string r(reinterpret_cast<char *>(dest), s);
       free(dest);
-      return r;
+      return std::pair(std::move(r), 0);
     } else {
       free(dest);
+      return std::pair(std::string(), static_cast<int>(s));
     }
   }
-  return std::string();
 }
 
 #endif
